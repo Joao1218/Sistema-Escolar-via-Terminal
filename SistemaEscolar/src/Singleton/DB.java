656e1d75fbb8f569d910390873c868c37a658df1
João Reinaldo <jreinaldo@abbc.org.br>
2025-07-30 12:15:44 -0300

    Atualizando a configuração e detalhes do banco

--- SistemaEscolar/src/Singleton/DB.java
+++ SistemaEscolar/src/Singleton/DB.java
@@ -1,26 +1,69 @@
 package Singleton;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import Singleton.Exception.DbException;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
 
 public class DB {
-
-    private String url;
-    private String user;
-    private String password;
-    private Connection conn;
-
-    public DB(String url, String user, String password, Connection conn) throws SQLException {
-        this.url = "jdbc:postgresql://" + System.getenv("DB_HOST") + ":" +
-                System.getenv("DB_PORT") + "/" + System.getenv("DB_NAME");
-        this.user = System.getenv("DB_USER");
-        this.password = System.getenv("DB_PASS");
-
-        this.conn = DriverManager.getConnection(url, user, password);
-    }
-
-    public Connection getConn() {
+    private static Connection conn = null;
+    public static Connection getConn() throws Exception {
+        if (conn == null) {
+            try {
+                Properties pros = loadProperties();
+                String url = pros.getProperty("dburl");
+                conn = DriverManager.getConnection(url, pros);
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
         return conn;
+    }
+    private static Properties loadProperties() throws Exception{
+        try(FileInputStream fs = new FileInputStream("db.properties")){
+            Properties props = new Properties();
+            props.load(fs);
+            return props;
+        }catch (IOException e){
+            throw new Exception(e.getMessage());
+        }
+    }
+    public static void closeConection(){
+        if(conn != null){
+            try {
+                conn.close();
+            }  catch(SQLException e){
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+    public static void closeStatement(Statement st){
+        if(st != null){
+            try{
+                st.close();
+            }catch (SQLException e){
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+    public static void closeResultSet(ResultSet rs){
+        if(rs != null){
+            try{
+                rs.close();
+            }catch (SQLException e){
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+    public static void closePrepareStatement(PreparedStatement st){
+        if(st != null){
+            try{
+                st.close();
+            }catch (SQLException e){
+                throw new DbException(e.getMessage());
+            }
+        }
     }
 }
---
+++ SistemaEscolar/src/Singleton/Exception/DbException.java
@@ -0,0 +1,7 @@
+package Singleton.Exception;
+
+public class DbException extends RuntimeException{
+    public DbException(String msg){
+        super(msg);
+    }
+}
